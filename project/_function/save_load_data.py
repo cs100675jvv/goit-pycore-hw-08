9bfd9c7331e4384d3d5b68d4a07dcb83d053bc37
cs100675jvv <cs100675jvv@gmail.com>
2024-05-19 15:25:26 +0300

    file in seperate folder

--- project/_function/save_load_data.py
+++ project/_function/save_load_data.py
@@ -1,11 +1,11 @@
 import pickle
 from _classes.adress_book import AddressBook
 
-def save_data(book, filename="._files/addressbook.pkl"):
+def save_data(book, filename="project/_files/addressbook.pkl"):
     with open(filename, "wb") as f:
         pickle.dump(book, f)
 
-def load_data(filename="._files/addressbook.pkl"):
+def load_data(filename="project/_files/addressbook.pkl"):
     try:
         with open(filename, "rb") as f:
             return pickle.load(f)
